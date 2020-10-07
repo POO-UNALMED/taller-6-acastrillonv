1aa02b5f79844e82cc7d1c8100ee043d7ece6da9
acastrillonv <48304499+acastrillonv@users.noreply.github.com>
2020-10-06 21:52:47 -0500

    Completo

--- src/main/java/vehiculos/Fabricante.java
+++ src/main/java/vehiculos/Fabricante.java
@@ -1,12 +1,23 @@
 package vehiculos;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Fabricante {
-	
+
 	private String nombre;
 	private Pais pais;
-		
+	private static List<Fabricante> listadof = new ArrayList<>();
+
 	public Fabricante(String nombre, Pais pais) {
+		super();
 		this.nombre = nombre;
 		this.pais = pais;
+		pais.setFabricantes(this);
+		listadof.add(this);
+	}
+	public Fabricante() {
+		listadof.add(this);
 	}
 
 	public String getNombre() {
@@ -25,8 +36,26 @@ public class Fabricante {
 		this.pais = pais;
 	}
 
-	public static Vehiculo fabricaMayorVentas() {
-		return null;
+	public List<Fabricante> getListadof() {
+		return listadof;
 	}
+
+	public static void setListadof(List<Fabricante> listadof) {
+		Fabricante.listadof = listadof;
+	}
+
+	public static Fabricante fabricaMayorVentas() {
+		int contador=0;
+		Fabricante fabricante=null;
+		for (Fabricante f:listadof) {
+			if(f.getListadof().size()>contador) {
+				contador=f.getListadof().size();
+				fabricante=f;
+		}
+		
+	}
+		return fabricante;
+	
+}
 	
 }
--- src/main/java/vehiculos/Pais.java
+++ src/main/java/vehiculos/Pais.java
@@ -1,12 +1,21 @@
 package vehiculos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pais {
 	private String nombre;
-	
+	private static List<Pais> paises = new ArrayList<>();
+	private  List<Fabricante> fabricantes = new ArrayList<>();
+
 	public Pais(String nombre) {
-		super();
 		this.nombre = nombre;
+		paises.add(this);
 	}
+	public Pais() {
+		paises.add(this);
+	}
+
 
 	public String getNombre() {
 		return nombre;
@@ -15,5 +24,38 @@ public class Pais {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	
+
+	public  List<Fabricante> getFabricantes() {
+		return fabricantes;
+	}
+
+	public static List<Pais> getPaises() {
+		return paises;
+	}
+	public static void setPaises(List<Pais> paises) {
+		Pais.paises = paises;
+	}
+	public void setFabricantes(Fabricante f) {
+		fabricantes.add(f);
+	}
+
+
+	public static Pais paisMasVendedor() {
+		int contador=0;
+
+		Pais pais=null;
+
+		for (Pais p:paises) {
+			if(p.getFabricantes().size()>contador) {
+				contador=p.getFabricantes().size();
+				pais=p;
+			}	
+		}
+		return pais;
+	}
+
+
+
 }
+
+
